a2ab9a81c3db34babad48cc56ce515a727c79a5e
Alex Rudenko <alexrudenko@chromium.org>
2026-07-03 15:27:32 +0200

    Remove dead code and remove skipped tests
    The code does not do anything right now so this CL is removing it and
    the related skipped test in favor of crbug.com/41363012.
    
    Bug: 41363012
    Change-Id: I4568e9bd553e1adb4c1e8c123a2ac82382ab0bd7
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/8040464
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

--- front_end/models/trace/handlers/ScreenshotsHandler.ts
+++ front_end/models/trace/handlers/ScreenshotsHandler.ts
@@ -7,21 +7,14 @@ import * as Types from '../types/types.js';
 
 import type {HandlerName} from './types.js';
 
-// Each thread contains events. Events indicate the thread and process IDs, which are
-// used to store the event in the correct process thread entry below.
-let unpairedAsyncEvents: Types.Events.PipelineReporter[] = [];
-
 let legacyScreenshotEvents: Types.Events.LegacyScreenshot[] = [];
 let modernScreenshotEvents: Types.Events.Screenshot[] = [];
 let syntheticScreenshots: Types.Events.LegacySyntheticScreenshot[] = [];
-let frameSequenceToTs: Record<string, Types.Timing.Micro> = {};
 
 export function reset(): void {
-  unpairedAsyncEvents = [];
   legacyScreenshotEvents = [];
   syntheticScreenshots = [];
   modernScreenshotEvents = [];
-  frameSequenceToTs = {};
 }
 
 export function handleEvent(event: Types.Events.Event): void {
@@ -29,39 +22,26 @@ export function handleEvent(event: Types.Events.Event): void {
     legacyScreenshotEvents.push(event);
   } else if (Types.Events.isScreenshot(event)) {
     modernScreenshotEvents.push(event);
-  } else if (Types.Events.isPipelineReporter(event)) {
-    unpairedAsyncEvents.push(event);
   }
 }
 
 export async function finalize(): Promise<void> {
-  const pipelineReporterEvents = Helpers.Trace.createMatchedSortedSyntheticEvents(unpairedAsyncEvents);
-
-  frameSequenceToTs = Object.fromEntries(pipelineReporterEvents.map(evt => {
-    const args = evt.args.data.beginEvent.args;
-    const frameReporter = 'frame_reporter' in args ? args.frame_reporter : args.chrome_frame_reporter;
-    const frameSequenceId = frameReporter.frame_sequence;
-    const presentationTs = Types.Timing.Micro(evt.ts + evt.dur);
-    return [frameSequenceId, presentationTs];
-  }));
-
   for (const snapshotEvent of legacyScreenshotEvents) {
     const {cat, name, ph, pid, tid} = snapshotEvent;
-    const syntheticEvent = Helpers.SyntheticEvents.SyntheticEventsManager.registerSyntheticEvent<
-        Types.Events.LegacySyntheticScreenshot>({
-      rawSourceEvent: snapshotEvent,
-      cat,
-      name,
-      ph,
-      pid,
-      tid,
-      // TODO(paulirish, crbug.com/41363012): investigate why getPresentationTimestamp(snapshotEvent) seems less accurate. Resolve screenshot timing inaccuracy.
-      // `getPresentationTimestamp(snapshotEvent) - snapshotEvent.ts` is how many microsec the screenshot should be adjusted to the right/later
-      ts: snapshotEvent.ts,
-      args: {
-        dataUri: `data:image/jpg;base64,${snapshotEvent.args.snapshot}`,
-      },
-    });
+    const syntheticEvent =
+        Helpers.SyntheticEvents.SyntheticEventsManager.registerSyntheticEvent<Types.Events.LegacySyntheticScreenshot>({
+          rawSourceEvent: snapshotEvent,
+          cat,
+          name,
+          ph,
+          pid,
+          tid,
+          // TODO(paulirish, crbug.com/41363012): fix snapshot timestamps.
+          ts: snapshotEvent.ts,
+          args: {
+            dataUri: `data:image/jpg;base64,${snapshotEvent.args.snapshot}`,
+          },
+        });
     syntheticScreenshots.push(syntheticEvent);
   }
 }
@@ -71,31 +51,6 @@ export function screenshotImageDataUri(event: Types.Events.LegacySyntheticScreen
     return event.args.dataUri;
   }
   return `data:image/jpg;base64,${event.args.snapshot}`;
-}
-
-/**
- * Correct the screenshot timestamps
- * The screenshot 'snapshot object' trace event has the "frame sequence number" attached as an ID.
- * We match that up with the "PipelineReporter" trace events as they terminate at presentation.
- * Presentation == when the pixels hit the screen. AKA Swap on the GPU
- */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function getPresentationTimestamp(screenshotEvent: Types.Events.LegacyScreenshot): Types.Timing.Micro {
-  const frameSequence = parseInt(screenshotEvent.id, 16);
-  // If it's 1, then it's an old trace (before https://crrev.com/c/4957973) and cannot be corrected.
-  if (frameSequence === 1) {
-    return screenshotEvent.ts;
-  }
-  // The screenshot trace event's `ts` reflects the "expected display time" which is ESTIMATE.
-  // It is set by the compositor frame sink from the `expected_display_time`, which is based on a previously known
-  // frame start PLUS the vsync interval (eg 16.6ms)
-  const updatedTs = frameSequenceToTs[frameSequence];
-  // Do we always find a match? No...
-  // We generally don't match the very first screenshot and, sometimes, the last
-  // The very first screenshot is requested immediately (even if nothing is painting). As a result there's no compositor
-  // instrumentation running alongside.
-  // The last one is sometimes missing as because the trace terminates right before the associated PipelineReporter is emitted.
-  return updatedTs ?? screenshotEvent.ts;
 }
 
 export interface Data {
