403b4202fa9c382d134b5c79ab250c217ad74506
Alex Rudenko <alexrudenko@chromium.org>
2026-07-06 08:55:11 +0200

    Remove liveedit e2e test
    Bug: 417884172
    Change-Id: I1735a5693f58fa37d8305491f26a36778bdbdfed
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/8034952
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>

--- test/e2e/resources/sources/live-edit-moving-breakpoint.js
+++
@@ -1,9 +0,0 @@
-async function foo() {
-  // Insertion marker for newline.
-  return true;
-}
-
-async () => {
-  const x = await foo();  // Breakpoint is set on this line.
-}
-const y = 25;  // Empty statement for the breakpoint to move to.
